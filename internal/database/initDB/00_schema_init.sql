8ea8316d26128eb797c39f1e74bb794ae758c26e
CodeAgha <amirmahdyjdx@gmail.com>
2025-08-15 22:52:19 +0330

    perf: remove extera indexes from sql schema

--- internal/database/initDB/00_schema_init.sql
+++ internal/database/initDB/00_schema_init.sql
@@ -2,9 +2,7 @@ CREATE TABLE IF NOT EXISTS posts (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   sender_id INT UNSIGNED NOT NULL,
   content TEXT NOT NULL,
-  created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  INDEX idx_sender_created (sender_id, created_at),
-  INDEX idx_created_at (created_at)
+  created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
