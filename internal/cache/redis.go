35fe389e3f99b7d78492e38e74d07b6992824b32
CodeAgha <amirmahdyjdx@gmail.com>
2025-08-21 03:29:26 +0330

    refactor: safer redis ZADD

--- internal/cache/redis.go
+++ internal/cache/redis.go
@@ -7,25 +7,25 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type TimelineCache interface {
-	AddPostToTimeline(ctx context.Context, userID uint, postID uint, score float64) error
-	GetTimelinePostIDs(ctx context.Context, userID uint, start, stop int64) ([]uint, error)
-}
-
-type timelineCache struct {
+type TimelineCache struct {
 	client *redis.Client
 }
 
-func New(client *redis.Client) TimelineCache {
-	return &timelineCache{client: client}
+func New(client *redis.Client) *TimelineCache {
+	return &TimelineCache{client: client}
 }
 
-func (tc *timelineCache) AddPostToTimeline(ctx context.Context, userID uint, postID uint, score float64) error {
+func (tc *TimelineCache) AddPostToTimeline(ctx context.Context, userID uint, postID uint, score float64) error {
 	key := timelineKey(userID)
-	return tc.client.ZAdd(ctx, key, redis.Z{Score: score, Member: postID}).Err()
+	err := tc.client.ZAdd(ctx, key, redis.Z{Score: score, Member: postID}).Err()
+	if err != nil {
+		err = fmt.Errorf("fail to add to ZSET %w", err)
+	}
+
+	return err
 }
 
-func (tc *timelineCache) GetTimelinePostIDs(ctx context.Context, userID uint, start, stop int64) ([]uint, error) {
+func (tc *TimelineCache) GetTimelinePostIDs(ctx context.Context, userID uint, start, stop int64) ([]uint, error) {
 	key := timelineKey(userID)
 	ids, err := tc.client.ZRevRange(ctx, key, start, stop).Result()
 	if err != nil {
@@ -41,15 +41,4 @@ func (tc *timelineCache) GetTimelinePostIDs(ctx context.Context, userID uint, st
 		result[i] = id
 	}
 	return result, nil
-}
-
-func NewRedisClient(addr string) *redis.Client {
-	return redis.NewClient(&redis.Options{
-		Addr: addr,
-		DB:   0,
-	})
-}
-
-func timelineKey(userID uint) string {
-	return fmt.Sprintf("timeline:%d", userID)
 }
