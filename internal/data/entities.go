cfb8fee30067dfea446da1132da9c7c74c4cbb89
CodeAgha <amirmahdyjdx@gmail.com>
2025-08-23 02:34:40 +0330

    refactor: add json tags to post struct

--- internal/data/entities.go
+++ internal/data/entities.go
@@ -3,10 +3,10 @@ package dataaccess
 import "time"
 
 type Post struct {
-	ID        uint      `db:"id"`
-	SenderID  uint      `db:"sender_id"`
-	Content   string    `db:"content"`
-	CreatedAt time.Time `db:"created_at"`
+	ID        uint      `db:"id" json:"id,omitempty"`
+	SenderID  uint      `db:"sender_id" json:"sender_id"`
+	Content   string    `db:"content" json:"content"`
+	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty"`
 }
 
 type SubscriberUser struct {
